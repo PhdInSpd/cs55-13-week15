e68168facf814a8752de884783e20fa444401721
Jose Alfredo Martinez <chaver.yadim@gmail.com>
2022-09-17 04:52:04 -0700

    changed react module to use className instead of class

--- pages/[id].js
+++ pages/[id].js
@@ -26,11 +26,11 @@ export default function Entry( { itemData } ) {
     return(
         <Layout>
             <h1>favorite ranchera: {itemData.id}</h1>   
-            <article class = "card col-6">
-                <div class = "card-body">            
-                    <h5 class="card-title">song: {itemData.song}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">artist: {itemData.artist}</h6>
-                    <a href = {itemData.link} class="card-link">youtube video</a>
+            <article className = "card col-6">
+                <div className = "card-body">            
+                    <h5 className ="card-title">song: {itemData.song}</h5>
+                    <h6 className ="card-subtitle mb-2 text-muted">artist: {itemData.artist}</h6>
+                    <a href = {itemData.link} className ="card-link">youtube video</a>
                 </div>
             </article>
         </Layout>
